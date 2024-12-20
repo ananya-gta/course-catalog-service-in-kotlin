ba5813c7b611065becf58aebc104cf7fea0d363c
ananya-gta <anannya.gta@gmail.com>
2024-12-20 14:20:41 +0530

    setting up different profiles in spring boot

--- src/main/kotlin/com/kotlinspring/service/GreetingsService.kt
+++ src/main/kotlin/com/kotlinspring/service/GreetingsService.kt
@@ -1,8 +1,14 @@
 package com.kotlinspring.service
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class GreetingsService {
-    fun retrieveGreeting(name : String) = "Hello $name"
+
+    // The lateinit modifier is a keyword in Kotlin, used to mark a variable as late-initialized.
+//     This is particularly useful for properties that cannot be initialized at the point of declaration but are guaranteed to be initialized before they are accessed
+    @Value("\${message}")
+    lateinit var message: String
+    fun retrieveGreeting(name : String) = "$name, $message"
 }
