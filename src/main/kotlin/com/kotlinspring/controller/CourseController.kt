f66bb94c9f1a1e0348b4a816c502e8f40dd9c95b
ananya-gta <anannya.gta@gmail.com>
2024-12-24 17:35:41 +0530

    build the DELETE endpoint for adding new course

--- src/main/kotlin/com/kotlinspring/controller/CourseController.kt
+++ src/main/kotlin/com/kotlinspring/controller/CourseController.kt
@@ -19,4 +19,11 @@ class CourseController(val courseService: CourseService) {
 
     @PutMapping("/{course_id}")
     fun updateCourse(@RequestBody courseDTO : CourseDTO, @PathVariable("course_id") courseId: Int) : CourseDTO = courseService.updateCourse(courseId, courseDTO)
+
+    @DeleteMapping("/{course_id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteCourse(@PathVariable("course_id") courseId : Int) {
+        courseService.deleteCourse(courseId)
+    }
+
 }
